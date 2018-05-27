b12a06767ccf4a48521d0441dbc96abbcec7fb07
Grzegorz Galios <galgreg@wp.pl>
2018-05-27 17:33:36 +0200

    Adds unit tests checking initialization correctness in Space class; Adds CrudeRendererTests class to test CrudeRenderer method; Adds some unit test in CrudeRendererTests class.

---
+++ DonkeysGOL/DonkeysGOL.Tests/DonkeysGOL.Console.Tests/CrudeRendererTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DonkeysGOL.Console.View;
+using NUnit.Framework;
+using System.Timers;
+
+namespace DonkeysGOL.Tests.DonkeysGOL.Console.Tests
+{
+    [TestFixture]
+    public class CrudeRendererTests
+    {
+        [Test]
+        public void PeriodicRenderWithNullArgument()
+        {
+            Assert.Throws<NullReferenceException>(() => CrudeRenderer.PeriodicRender(null));
+        }
+
+        [Test]
+        public void RenderStateWithNullArgument()
+        {
+            Assert.Throws<NullReferenceException>(() => CrudeRenderer.RenderState(null));
+        }
+    }
+}
